21f10c9bb9555268c5d5aa34ef047c8276a70b3c
Riccardo Cadei <riccardo.cadei98@gmail.com>
2020-12-08 19:20:18 +0100

    update plots

--- plots.py
+++ plots.py
@@ -2,6 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_train_val(m_train, m_val, period=25, al_param=False, metric='IoU'):
+    """Plot the evolution of the metric evaluated on the training  and validation set during the trainining
+
+    Args:
+        m_train: history of the metric evaluated on the train 
+        m_val: history of the metric evaluated on the val 
+        period: number of epochs between 2 valutation of the train
+        al_param: number of epochs for each learning rate
+        metric: metric used (e.g. Loss, IoU, Accuracy)
+    Returns:
+        plot
+    """
     
     plt.title('Evolution of the '+metric+ ' with respect to the number of epochs',fontsize=14)
     
